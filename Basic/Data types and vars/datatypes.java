449989aca04b1d9fb1dc1f3fba63953c7666f8b8
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-13 21:31:11 +0530

    Java started

---
+++ Basic/Data types and vars/datatypes.java
@@ -0,0 +1,20 @@
+class datatypes {
+    public static void main(String args[]) {
+        byte age = 25;
+        int salary = 50000;
+        long distance = 123456789L;
+        float price = 99.99f;
+        double temperature = 36.6;
+        char grade = 'A';
+        boolean isJavaFun = true;
+        String name = "Vamshi";
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Distance: " + distance);
+        System.out.println("Price: " + price);
+        System.out.println("Temperature: " + temperature);
+        System.out.println("Grade: " + grade);
+        System.out.println("Java is fun: " + isJavaFun);
+        System.out.println("Name : " + name);
+    }
+}
