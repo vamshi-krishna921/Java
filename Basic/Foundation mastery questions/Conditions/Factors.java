c4a19504a9fc04ced7ad1d7e77a6651f6683dbbc
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-18 19:57:56 +0530

    Factors

---
+++ Basic/Foundation mastery questions/Conditions/Factors.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Factors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int number = sc.nextInt();
+        sc.nextLine();
+        for(int i = 1; i <= number; i++){
+            if (number%i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
