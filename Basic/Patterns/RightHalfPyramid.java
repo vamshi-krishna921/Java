f1179bb5a82fef81cc25e9e569430feccda861b0
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-23 06:31:45 +0530

    Right half pyramid

---
+++ Basic/Patterns/RightHalfPyramid.java
@@ -0,0 +1,13 @@
+public class RightHalfPyramid {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int space = 5-i; space > 0; space--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
