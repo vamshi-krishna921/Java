2833993ffe24391e6a862b2cabd1db09cd480abc
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-24 07:24:09 +0530

    Final change

--- Basic/Patterns/InvertedAlphabeticTriangle.java
+++ Basic/Patterns/InvertedAlphabeticTriangle.java
@@ -1,12 +1,26 @@
 public class InvertedAlphabeticTriangle {
     public static void main(String[] args) {
+
+        // * with no space
+        // for (int i = 1; i <= 5; i++) {
+        // for (int space = 0; space < i; space++) {
+        // System.out.print(" ");
+        // }
+        // char alpha = (char) ('I' - (2 * i) + 2);
+        // for (char j = alpha; j >= 'A'; j--) {
+        // System.out.print(j);
+        // }
+        // System.out.println();
+        // }
+
+        // * With space
         for (int i = 1; i <= 5; i++) {
-            for (int space = 0; space < i; space++) {
+            for (int space = 0; space <= (2 * i) - 2; space++) {
                 System.out.print(" ");
             }
             char alpha = (char) ('I' - (2 * i) + 2);
             for (char j = alpha; j >= 'A'; j--) {
-                System.out.print(j);
+                System.out.print(j + " ");
             }
             System.out.println();
         }
--- Basic/Patterns/InvertedFullTriangle.java
+++ Basic/Patterns/InvertedFullTriangle.java
@@ -1,11 +1,24 @@
 public class InvertedFullTriangle {
     public static void main(String[] args) {
+
+        // * With no space
+        // for (int i = 1; i <= 5; i++) {
+        // for (int space = 0; space <= i; space++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 7; j >= (2 * i) - 1; j--) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // * With space
         for (int i = 1; i <= 5; i++) {
-            for (int space = 0; space <= i; space++) {
+            for (int space = 0; space <= (i * 2) - 2; space++) {
                 System.out.print(" ");
             }
-            for (int j = 7; j >= (2 * i) -  1; j--) {
-                System.out.print("*");
+            for (int j = 7; j >= (2 * i) - 1; j--) {
+                System.out.print("* ");
             }
             System.out.println();
         }
