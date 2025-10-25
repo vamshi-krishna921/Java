b0cb39b863883e8d5271c90519e5ba22491be2a8
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-25 18:41:41 +0530

    zero one triangle

---
+++ Basic/Patterns/Zero1Triangle.java
@@ -0,0 +1,17 @@
+public class Zero1Triangle {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int space = 5; space >= i; space--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                if (j % 2 == 0) {
+                    System.out.print(0);
+                } else {
+                    System.out.print(1);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
