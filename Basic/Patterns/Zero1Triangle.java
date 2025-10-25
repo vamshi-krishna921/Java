79f16ec838eb631de12c8a7f7c0fb207db554294
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-25 19:06:10 +0530

    Repeated numbers

--- Basic/Patterns/Zero1Triangle.java
+++ Basic/Patterns/Zero1Triangle.java
@@ -1,7 +1,7 @@
 public class Zero1Triangle {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++) {
-            for (int space = 5; space >= i; space--) {
+            for (int space = 4; space >= i; space--) {
                 System.out.print(" ");
             }
             for (int j = 1; j <= (2 * i) - 1; j++) {
