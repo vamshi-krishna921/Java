93d7ff2f1b90e57366dab78545337fb1100c8111
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-26 10:34:30 +0530

    Rhombus

---
+++ Basic/Patterns/Rhombus.java
@@ -0,0 +1,25 @@
+public class Rhombus {
+    public static void main(String[] args) {
+        int n = 3;
+        // * Upper part
+        for (int i = 1; i <= n; i++) {
+            for (int space = n; space >= i; space--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // * Lower Part
+        for (int i = 2; i >= 1; i--) {
+            for (int space = n ; space >= i; space--) {
+                System.out.print(" ");
+            }
+            for (int j = (2 * i + 1); j >= n; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
