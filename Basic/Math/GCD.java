f9b96244369375954e5e4c7b70a3bb6692cd202d
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-29 08:01:27 +0530

    Euclidian algorithm

--- Basic/Math/GCD.java
+++ Basic/Math/GCD.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class GCD {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -7,11 +8,26 @@ public class GCD {
         System.out.print("Enter the second number : ");
         int num2 = sc.nextInt();
         int gcd = 0;
-        for (int i = 1; i <= num2; i++) {
-            if (num1 % i == 0 && num2 % i ==0) {
-                gcd = i;
+        // for (int i = 1; i <= num2; i++) {
+        // if (num1 % i == 0 && num2 % i ==0) {
+        // gcd = i;
+        // }
+        // }
+        // System.out.println(gcd);
+
+        // Using euclidian algorithm
+        // * gcd(a,b) = gcd(a%b,b);
+        while (num1 > 0 && num2 > 0) {
+            if (num1 > num2) {
+                num1 = num1 % num2;
+            }else{
+                num2 = num2 % num1;
             }
         }
-        System.out.println(gcd);
+        if (num1 == 0) {
+            System.out.println(num2);
+        }else{
+            System.out.println(num1);
+        }
     }
 }
