21a8857a86475493c0ed18f78635547084abf287
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-29 08:22:19 +0530

    Euclidian algorithm

--- Basic/Math/GCD.java
+++ Basic/Math/GCD.java
@@ -7,11 +7,11 @@ public class GCD {
         int num1 = sc.nextInt();
         System.out.print("Enter the second number : ");
         int num2 = sc.nextInt();
-        int gcd = 0;
+        // int gcd = 0;
         // for (int i = 1; i <= num2; i++) {
         // if (num1 % i == 0 && num2 % i ==0) {
         // gcd = i;
-        // }
+        // } 
         // }
         // System.out.println(gcd);
 
