ae85ac585e0ff6339dab12d7bf559eebd79abafe
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-29 11:38:57 +0530

    LCM

--- Basic/Math/GCD.java
+++ Basic/Math/GCD.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class GCD {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -8,14 +7,14 @@ public class GCD {
         System.out.print("Enter the second number : ");
         int num2 = sc.nextInt();
         // int gcd = 0;
-        // for (int i = 1; i <= num2; i++) {
+        // for (int i = 1; i <= Math.min(num1, num2); i++) {
         // if (num1 % i == 0 && num2 % i ==0) {
         // gcd = i;
         // } 
         // }
         // System.out.println(gcd);
 
-        // Using euclidian algorithm
+        //! Using Euclidian algorithm
         // * gcd(a,b) = gcd(a%b,b);
         while (num1 > 0 && num2 > 0) {
             if (num1 > num2) {
---
+++ Basic/Math/LCM.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class LCM {
+    public static void main(String[] args) {
+        // * Formula lcm = (num1 * num2) / GCD
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number : ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number : ");
+        int num2 = sc.nextInt();
+        int a = num1;
+        int b = num2;
+        // while (num1 > 0 && num2 > 0) {
+        // if (num1 > num2) {
+        // num1 = num1 % num2;
+        // } else {
+        // num2 = num2 % num1;
+        // }
+        // }
+        // int gcd = (num1 == 0) ? num2 : num1;
+        // int lcm = (a * b) / gcd;
+        // System.out.println(lcm);
+
+        // ! Without formula
+        int lcm = 0;
+        int maxNum = Math.max(num1, num2);
+        while (true) {
+            if (maxNum % num1 == 0 && maxNum % num2 == 0) {
+                lcm = maxNum;
+                break;
+            }
+            maxNum++;
+        }
+        System.out.println(lcm);
+    }
+}
