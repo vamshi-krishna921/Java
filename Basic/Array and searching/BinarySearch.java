37b73d3a2c976c7d0dcfb5991ef4dfc0d17dcc87
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-12-12 20:10:33 +0530

    Searching

---
+++ Basic/Array and searching/BinarySearch.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+    static int binarySearch(int[] arr, int key) {
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == key)
+                return mid;
+            else if (key < arr[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter the " + n + " elements : ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter the key element : ");
+        int key = sc.nextInt();
+        System.out.print("The key is at index : " + binarySearch(arr, key));
+    }
+}
