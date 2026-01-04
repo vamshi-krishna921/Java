baf4ac7e5b1ffe04cd4a13a9f829ae0cf36e3a6e
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2026-01-04 21:08:37 +0530

    Longest consecutive array

--- Strivers Sheet A-Z/Arrays/Medium/LongestConsecutive.java
+++ Strivers Sheet A-Z/Arrays/Medium/LongestConsecutive.java
@@ -1,28 +1,50 @@
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class LongestConsecutive {
-    static boolean linearSearch(int elem, int[] arr){
-        for (int num : arr) {
-            if (num == elem) {
-                return true;
-            }
-        }
-        return false;
-    }
+    // static boolean linearSearch(int elem, int[] arr){
+    // for (int num : arr) {
+    // if (num == elem) {
+    // return true;
+    // }
+    // }
+    // return false;
+    // }
 
     static int LongConsutive(int[] arr) {
         int maxLen = 0;
+        // for (int i = 0; i < arr.length; i++) {
+        // int elem = arr[i];
+        // int count = 1;
+        // while (linearSearch(elem+1, arr)) {
+        // elem++;
+        // count++;
+        // }
+        // maxLen = Math.max(maxLen, count);
+        // }
+        HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
-            int elem = arr[i];
-            int count = 1;
-            while (linearSearch(elem+1, arr)) {
-                elem++;
-                count++;
-            }
-            maxLen = Math.max(maxLen, count);
+            set.add(arr[i]);
         }
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i];
+            if (!set.contains(num - 1)) {
+                int count = 1;
+                int elem = num;
+                while (set.contains(elem + 1)) {
+                    elem++;
+                    count++;
+                }
+                maxLen = Math.max(maxLen, count);
+            }
+        }
+
         return maxLen;
     }
+
+    // TODO Optimal soln using Set
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
