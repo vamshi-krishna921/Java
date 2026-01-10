089a294dac6fd6df5a04f5e443b86c2425270f35
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2026-01-10 05:54:09 +0530

    Majority elem optimal

--- Strivers Sheet A-Z/Arrays/Hard/MajorityElem.java
+++ Strivers Sheet A-Z/Arrays/Hard/MajorityElem.java
@@ -20,14 +20,48 @@ public class MajorityElem {
         // }
 
         // * Better soln using the hashmap
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for (int i = 0; i < arr.length; i++) {
+        // map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        // if (map.get(arr[i]) > arr.length / 3) {
+        // ans.add(arr[i]);
+        // }
+        // }
 
-            if (map.get(arr[i]) > arr.length / 3) {
-                ans.add(arr[i]);
+        // TODO Optimal using the intution of n/2
+        int count1 = 0, count2 = 0;
+        int elem1 = 0, elem2 = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (count1 == 0 && elem2 != arr[i]) {
+                count1 = 1;
+                elem1 = arr[i];
+            }else if (count2 == 0 && elem1 != arr[i]) {
+                count2 = 1;
+                elem2 = arr[i];
+            }else if (arr[i] == elem1) {
+                count1++;
+            }else if (arr[i] == elem2) {
+                count2++;
+            }else{
+                count1--;
+                count2--;
             }
-        } 
+        }
+        count1 = 0;count2 = 0;
+        for (int elem : arr) {
+            if (elem1 == elem) {
+                count1++;
+            }
+            if (elem2 == elem) {
+                count2++;
+            }
+        }
+        if (count1 > arr.length/3) {
+            ans.add(elem1);
+        }
+        if (count2 > arr.length/3) {
+            ans.add(elem2);
+        }
 
         return ans;
     }
