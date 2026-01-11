2ab13284885d8c01b26df764a021f5c6420479fe
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2026-01-11 08:54:55 +0530

    3 sum completed

--- Strivers Sheet A-Z/Arrays/Hard/ThreeSum.java
+++ Strivers Sheet A-Z/Arrays/Hard/ThreeSum.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Scanner;
@@ -7,42 +8,74 @@ import java.util.Set;
 public class ThreeSum {
 
     static ArrayList<ArrayList<Integer>> sumThree(int[] arr, int size) {
-        ArrayList<Integer> list = new ArrayList<>();
         Set<ArrayList<Integer>> newSet = new HashSet<>();
         // ! Brute force using set
         // for (int i = 0; i < arr.length; i++) {
-        //     for (int j = i + 1; j < arr.length; j++) {
-        //         for (int k = j + 1; k < arr.length; k++) {
-        //             if (arr[i] + arr[j] + arr[k] == 0) {
-        //                 ArrayList<Integer> temp = new ArrayList<>();
-        //                 temp.add(arr[i]);
-        //                 temp.add(arr[j]);
-        //                 temp.add(arr[k]);
-        //                 Collections.sort(temp);
-        //                 newSet.add(temp);
-        //             }
-        //         }
-        //     }
+        // for (int j = i + 1; j < arr.length; j++) {
+        // for (int k = j + 1; k < arr.length; k++) {
+        // if (arr[i] + arr[j] + arr[k] == 0) {
+        // ArrayList<Integer> temp = new ArrayList<>();
+        // temp.add(arr[i]);
+        // temp.add(arr[j]);
+        // temp.add(arr[k]);
+        // Collections.sort(temp);
+        // newSet.add(temp);
+        // }
+        // }
+        // }
         // }
 
-        //* Better soln using set
-        Set<Integer> tempSet = new HashSet<>();
+        // * Better soln using set
+        // Set<Integer> tempSet = new HashSet<>();
+        // for (int i = 0; i < arr.length; i++) {
+        // tempSet.clear();
+        // for (int j = i + 1; j < arr.length; j++) {
+        // int nextElem = -(arr[i] + arr[j]);
+        // if (tempSet.contains(nextElem)) {
+        // ArrayList<Integer> tempList = new ArrayList<>();
+        // tempList.add(arr[i]);
+        // tempList.add(arr[j]);
+        // tempList.add(nextElem);
+        // Collections.sort(tempList);
+        // newSet.add(tempList);
+        // } else {
+        // tempSet.add(arr[j]);
+        // }
+
+        // }
+        // }
+
+        // TODO Optimal solution using 3 pointer approach
+        Arrays.sort(arr);
         for (int i = 0; i < arr.length; i++) {
-            tempSet.clear();
-            for (int j = i+1; j < arr.length; j++) {
-                int nextElem = -(arr[i] +  arr[j]);
-                if (tempSet.contains(nextElem)) {
-                    ArrayList<Integer> tempList = new ArrayList<>();
-                    tempList.add(arr[i]);
-                    tempList.add(arr[j]);
-                    tempList.add(nextElem);
-                    Collections.sort(tempList);
-                    newSet.add(tempList);
-                }else{
-                    tempSet.add(arr[j]);
-                }
-                
+            int j = i + 1;
+            int k = arr.length - 1;
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                continue;
             }
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    ArrayList<Integer> temp = new ArrayList<>();
+                    temp.add(arr[i]);
+                    temp.add(arr[j]);
+                    temp.add(arr[k]);
+                    newSet.add(temp);
+                    while (j < k && arr[j] == arr[j+1]) {
+                        j++;
+                    }
+                    while (j < k && arr[k] == arr[k - 1]) {
+                        k--;
+                    }
+                    j++;
+                    k--;
+                }
+            }
+
         }
         return new ArrayList<>(newSet);
         // return list;
