1e2a1f2dce6235f089ee988af15a7847e50e062c
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2026-01-11 21:26:10 +0530

    4Sum completed

--- Strivers Sheet A-Z/Arrays/Hard/fourSum.java
+++ Strivers Sheet A-Z/Arrays/Hard/fourSum.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Scanner;
@@ -7,23 +8,83 @@ import java.util.Set;
 public class fourSum {
     static ArrayList<ArrayList<Integer>> foursum(int arr[], int target) {
         Set<ArrayList<Integer>> newSet = new HashSet<>();
+
+        // ! Brute force using set and list
+        // for (int i = 0; i < arr.length; i++) {
+        // for (int j = i + 1; j < arr.length; j++) {
+        // for (int k = j + 1; k < arr.length; k++) {
+        // for (int l = k + 1; l < arr.length; l++) {
+        // if (arr[i] + arr[j] + arr[k] + arr[l] == target) {
+        // ArrayList<Integer> tempList = new ArrayList<>();
+        // tempList.add(arr[i]);
+        // tempList.add(arr[j]);
+        // tempList.add(arr[k]);
+        // tempList.add(arr[l]);
+        // Collections.sort(tempList);
+        // newSet.add(tempList);
+        // }
+        // }
+        // }
+        // }
+        // }
+
+        // * Better soln
+        // for (int i = 0; i < arr.length; i++) {
+        // for (int j = i + 1; j < arr.length; j++) {
+        // Set<Long> tempSet = new HashSet<>();
+        // for (int k = j + 1; k < arr.length; k++) {
+        // long sum = (long) arr[i] + arr[j] + arr[k];
+        // long fourth = (long) target - sum;
+        // if (tempSet.contains(fourth)) {
+        // ArrayList<Integer> tempList = new ArrayList<>();
+        // tempList.add(arr[i]);
+        // tempList.add(arr[j]);
+        // tempList.add(arr[k]);
+        // tempList.add((int) fourth);
+        // Collections.sort(tempList);
+        // newSet.add(tempList);
+        // }
+        // tempSet.add((long) arr[k]);
+        // }
+        // }
+        // }
+
+        // TODO 2 pointers
+        Arrays.sort(arr);
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
-                for (int k = j + 1; k < arr.length; k++) {
-                    for (int l = k + 1; l < arr.length; l++) {
-                        if (arr[i] + arr[j] + arr[k] + arr[l] == target) {
-                            ArrayList<Integer> tempList = new ArrayList<>();
-                            tempList.add(arr[i]);
-                            tempList.add(arr[j]);
-                            tempList.add(arr[k]);
-                            tempList.add(arr[l]);
-                            Collections.sort(tempList);
-                            newSet.add(tempList);
+                if (j != i + 1 && arr[j] == arr[j - 1]) {
+                    continue;
+                }
+                int k = j + 1;
+                int l = arr.length - 1;
+                while (k < l) {
+                    long sum = (long) arr[i] + arr[j] + arr[k] + arr[l];
+                    if (sum == target) {
+                        ArrayList<Integer> tempList = new ArrayList<>();
+                        tempList.add(arr[i]);
+                        tempList.add(arr[j]);
+                        tempList.add(arr[k]);
+                        tempList.add(arr[l]);
+                        newSet.add(tempList);
+                        while (k < l && arr[k] == arr[k + 1]) {
+                            k++;
                         }
+                        while (k < l && arr[l] == arr[l - 1]) {
+                            l--;
+                        }
+                        k++;
+                        l--;
+                    }else if (sum<target) {
+                        k++;
+                    }else{
+                        l--;
                     }
                 }
+
             }
         }
+
         return new ArrayList<>(newSet);
     }
 
