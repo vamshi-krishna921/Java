c96774fca11e8d441bc1bf1e7bd638ee7dd2af64
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2026-01-19 22:48:59 +0530

    Reverse words

--- Strivers Sheet A-Z/Strings/Easy/ReverseWords.java
+++ Strivers Sheet A-Z/Strings/Easy/ReverseWords.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class ReverseWords {
-    
+
+    static String reverseStr(String str) {
+        String[] string = str.split(" +");
+        int left = 0;
+        int right = string.length - 1;
+        while (left < right) {
+            String word = string[left];
+            string[left] = string[right];
+            string[right] = word;
+            left++;
+            right--;
+        }
+        str = String.join(" ", string);
+        return str.trim();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        String result = reverseStr(str);
+        System.out.println(result);
+    }
 }
