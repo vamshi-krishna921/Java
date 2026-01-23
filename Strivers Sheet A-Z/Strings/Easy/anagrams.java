f8a93fdc6a96bc581d8eb55c7ddf2c348fdfc64a
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2026-01-23 20:43:55 +0530

    Anagrams

--- Strivers Sheet A-Z/Strings/Easy/anagrams.java
+++ Strivers Sheet A-Z/Strings/Easy/anagrams.java
@@ -6,23 +6,38 @@ public class anagrams {
         if (s.length() != t.length()) {
             return false;
         }
-        // *Better soln using hasmap
-        HashMap<Character, Integer> map = new HashMap<>();
+        // *Better soln using hashmap()
+        // HashMap<Character, Integer> map = new HashMap<>();
+        // for (int i = 0; i < s.length(); i++) {
+        //     char ch1 = s.charAt(i);
+        //     map.put(ch1, map.getOrDefault(ch1, 0) + 1);
+        // }
+        // for (int i = 0; i < t.length(); i++) {
+        //     char ch2 = t.charAt(i);
+        //     if (!map.containsKey(ch2)) {
+        //         return false;
+        //     }
+        //     map.put(ch2, map.getOrDefault(ch2, 0) - 1);
+        //     if (map.get(ch2) == 0) {
+        //         map.remove(ch2);
+        //     }
+        // }
+        // return map.isEmpty();
+
+        //TODO Optimal using frequency count
+        int[] arr = new int[26];
         for (int i = 0; i < s.length(); i++) {
             char ch1 = s.charAt(i);
-            map.put(ch1, map.getOrDefault(ch1, 0) + 1);
-        }
-        for (int i = 0; i < t.length(); i++) {
             char ch2 = t.charAt(i);
-            if (!map.containsKey(ch2)) {
+            arr[ch1 - 'a']++;
+            arr[ch2 - 'a']--; 
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]!=0) {
                 return false;
             }
-            map.put(ch2, map.getOrDefault(ch2, 0) - 1);
-            if (map.get(ch2) == 0) {
-                map.remove(ch2);
-            }
         }
-        return map.isEmpty();
+        return true;
     }
 
     public static void main(String[] args) {
