663e50e3df5dce7fcbc5dc8ad3134a59b86b7843
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2026-02-04 19:50:23 +0530

    Single element in sorted array

---
+++ Strivers Sheet A-Z/Binary Search/BS 1D-Arrays/singleElement.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class singleElement {
+    static int search(int[] arr){
+        if (arr.length == 1) {
+            return arr[0];
+        }
+        int low = 0, high = arr.length-1;
+        if (arr[low] != arr[low+1]) {
+            return arr[low];
+        }
+        if (arr[high] != arr[high-1]) {
+            return arr[high];
+        }
+        while (low<=high) {
+            int mid = low + (high-low)/2;
+            if ((mid%2==1 && arr[mid] == arr[mid -1]) && mid%2==0 && arr[mid] == arr[mid+1]) {
+                low = mid+1;
+            }else{
+                high = mid -1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.print("Enter the " + size + " elements: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("The upper bound found at index : " + search(arr));
+    }
+}
