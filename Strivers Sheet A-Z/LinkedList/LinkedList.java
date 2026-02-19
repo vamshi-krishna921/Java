c7a263b9af3c218a56e7a6d2b9fae549a8df90a2
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2026-02-19 21:34:31 +0530

    Singly linkedList Adding the elem in differnt ways

--- Strivers Sheet A-Z/LinkedList/LinkedList.java
+++ Strivers Sheet A-Z/LinkedList/LinkedList.java
@@ -24,7 +24,19 @@ public class LinkedList {
         }
         tail.next = newNode;
         tail = newNode;
- 
+
+    }
+
+    // * First order sequence
+    public void firstOrder(int data) {
+        node newNode = new node(data);
+        if (head == null) {
+            head = tail = newNode;
+            head.next = null;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
     }
 
     // * Printing list
@@ -37,9 +49,60 @@ public class LinkedList {
                 System.out.print(temp.data + " -> ");
                 temp = temp.next;
             }
-            System.out.print("null");
+            System.out.println("null");
         }
 
+    }
+
+    // * Add first
+    public static void addFirst(int data) {
+        node newNode = new node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // * Add Last
+    public static void addLast(int data) {
+        node newNode = new node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    // * Add at position
+    public static void addAtPos(int pos, int data) {
+        node newNode = new node(data);
+        if (pos == 1) {
+            newNode.next = head;
+            head = newNode;
+
+            if (tail == null) {
+                tail = newNode;
+            }
+            return;
+        }
+        node temp = head;
+        int count = 1;
+        while (temp != null && count < pos - 1) {
+            temp = temp.next;
+            count++;
+        }
+        if (temp == null) {
+            System.out.println("Position out of bounds");
+            return;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+        if (newNode.next == null) {
+            tail = newNode;
+        }
     }
 
     public static void main(String[] args) {
@@ -48,7 +111,11 @@ public class LinkedList {
         while (true) {
             System.out.println("1. Add elem ");
             System.out.println("2. Print list");
+            System.out.println("3. Add elem first order");
             System.out.println("4. Exit");
+            System.out.println("5. Add at first");
+            System.out.println("6. Add elem at last");
+
             int x = sc.nextInt();
             switch (x) {
                 case 1:
@@ -58,8 +125,31 @@ public class LinkedList {
                     break;
                 case 2:
                     ll.printList();
+                    break;
+                case 3:
+                    System.out.print("Enter the value: ");
+                    val = sc.nextInt();
+                    ll.firstOrder(val);
+                    break;
                 case 4:
                     System.exit(0);
+                case 5:
+                    System.out.print("Enter the value: ");
+                    val = sc.nextInt();
+                    ll.addFirst(val);
+                    break;
+                case 6:
+                    System.out.print("Enter the value: ");
+                    val = sc.nextInt();
+                    ll.addLast(val);
+                    break;
+                case 7:
+                    System.out.print("Enter the position: ");
+                    int pos = sc.nextInt();
+                    System.out.print("Enter the elem: ");
+                    val = sc.nextInt();
+                    ll.addAtPos(pos, val);
+                    break;
                 default:
                     break;
             }
