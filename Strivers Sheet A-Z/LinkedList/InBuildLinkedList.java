554ac94c4d23522cc83bae70d103304e1ca7f842
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2026-02-24 22:07:07 +0530

    Inbuild linkedList methods

--- Strivers Sheet A-Z/LinkedList/InBuildLinkedList.java
+++ Strivers Sheet A-Z/LinkedList/InBuildLinkedList.java
@@ -1,3 +1,17 @@
+import java.util.LinkedList;
+
 public class InBuildLinkedList {
-    
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Ramu");
+        list.add("Hanuman");
+        list.add(1, "Krishna");
+        // System.out.println(list.get(1));
+        list.set(0, "Shiva");
+        // System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+        list.remove("Shiva");
+        System.out.println(list);
+    }
 }
