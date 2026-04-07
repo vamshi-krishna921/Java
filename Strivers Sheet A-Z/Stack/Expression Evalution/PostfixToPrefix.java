81235cf90406dd417089975843ab582596858e71
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2026-04-07 19:04:03 +0530

    Postfix to Prefix

---
+++ Strivers Sheet A-Z/Stack/Expression Evalution/PostfixToPrefix.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class PostfixToPrefix {
+    public static String convert(String str) {
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                stack.push(ch + "");
+            } else {
+                String op2 = stack.pop();
+                String op1 = stack.pop();
+                String temp = ch + op1 + op2;
+                stack.push(temp);
+            }
+        }
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        String exp = "AB+C*";
+        System.out.println(convert(exp));
+    }
+}
