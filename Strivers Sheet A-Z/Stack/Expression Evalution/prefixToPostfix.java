25fa1464f55d04e2c63da07c052b51ccc1808979
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2026-04-23 07:44:49 +0530

    Prefix to infix,postfix

---
+++ Strivers Sheet A-Z/Stack/Expression Evalution/prefixToPostfix.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class prefixToPostfix {
+    public static String convert(String str) {
+        Stack<String> stack = new Stack<>();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char ch = str.charAt(i);
+            if (Character.isLetterOrDigit(ch)) {
+                stack.push(ch + "");
+            } else {
+                String op1 = stack.pop();
+                String op2 = stack.pop();
+                String temp = op1 + op2 + ch;
+                stack.push(temp);
+            }
+        }
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        String exp = "-+A*BC/DE";
+        System.out.println(convert(exp));
+    }
+}
